3378f93d7e91449210900d45cf08dd1b907cfd24
Olha Shekhet <oliayesss@gmail.com>
2023-04-25 17:35:59 -0700

    Add a bunch of JS tasks

---
+++ js-tasks/2623-memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+  const answer = {}
+
+  return function(...args) {
+      const argsString = args.join(".")
+      const cacheValue = answer[argsString]
+
+      if(cacheValue !== undefined) {
+          return cacheValue
+      }
+
+      return answer[argsString] = fn(...args)
+  }
+}
+
+
+/** 
+* let callCount = 0;
+* const memoizedFn = memoize(function (a, b) {
+*	 callCount += 1;
+*   return a + b;
+* })
+* memoizedFn(2, 3) // 5
+* memoizedFn(2, 3) // 5
+* console.log(callCount) // 1 
+*/
